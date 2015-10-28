d5a01e4bd92271bba3b86ef3f94c1ac71158ada9
marwa <marwa@192.168.10.29>
2015-10-28 11:42:29 +0100

    second commit
    Signed-off-by: marwa <marwa@192.168.10.29>

--- fibonacci/src/main/java/edu/esprit/fibonacci/Fibonacci.java
+++ fibonacci/src/main/java/edu/esprit/fibonacci/Fibonacci.java
@@ -2,7 +2,8 @@ package edu.esprit.fibonacci;
 
 public class Fibonacci {
 
-	
+	//Hello this is the new added code after the push 
+	//now we will see if github will detect the difference 
 	public String sayHello(String nom){
 		if (nom.equals("Man"))
 			throw new IllegalArgumentException();
